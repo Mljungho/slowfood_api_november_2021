1c6cc9e132a852db09d3a5cb461f75e1c47d2a2e
Mljungho <mathias.ljungholm@gmail.com>
2021-12-07 14:36:16 +0100

    db:migration create_user & add_user_to_orders. implemented order method for orders_controller Wrote Order & User_spec - test passes when run Rspec.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,15 @@
 class Order < ApplicationRecord
+  belongs_to :user
+end
+
+describe 'Database Table' do
+    
+end
+
+describe 'Associations'do
+  
+end
+
+describe 'Factory' do
+  
 end
--- spec/requests/api/orders/create_spec.rb
+++ spec/requests/api/orders/create_spec.rb
@@ -3,11 +3,27 @@
 #  3. We need to be able to create an Order and connect/associate it with the User
 #  4. We need to associate the Order with a Product using a join table that we will call OrderItem
 
-RSpec.describe 'POST /api/orders' do
+RSpec.describe 'POST /api/orders', type: :request do
+  let!(:user) { create(:user) }
+
   subject { response }
+
   before do
-    post '/api/orders', params: { order: { productI_id: 1, user_id: 1 } }
+    post '/api/orders', params: {
+      order: {
+        productI_id: 1, user_id: user.id
+      }
+    }
+    @order = Order.last
   end
 
   it { is_expected.to have_http_status :created }
+
+  it 'is expected to create an instance of Order' do
+    expect(Order.last).to_not eq nil
+  end
+
+  it 'is expected to associate order with user' do
+    expect(@order.user).to eq user
+  end
 end
